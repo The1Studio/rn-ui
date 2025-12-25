219539a1f0150d66d59c70a31838c57ed1287b45
QuanNguyen <quan.nguyen.2629@gmail.com>
2025-12-25 16:30:17 +0700

    Fix Collapsible card

--- src/components/CollapsibleCard.tsx
+++ src/components/CollapsibleCard.tsx
@@ -128,6 +128,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: colors.border,
     overflow: 'hidden',
+    width: '100%',
   },
   containerDisabled: {
     opacity: 0.6,
